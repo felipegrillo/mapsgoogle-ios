69427d62a1bebc9d3228fee5c4acf53fd02c1ac8
lab04 <ieepo@felipe.local>
2015-02-08 12:00:17 -0600

    Initial Commit

---
+++ mapsgoogle-ios/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mapsgoogle-ios
+//
+//  Created by felipe on 08/02/15.
+//  Copyright (c) 2015 felipe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ mapsgoogle-ios/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mapsgoogle-ios
+//
+//  Created by felipe on 08/02/15.
+//  Copyright (c) 2015 felipe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
